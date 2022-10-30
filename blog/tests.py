73e789677de58024ac7b0047daff9bb686ee143a
Eya <94762397+eyabenltaief@users.noreply.github.com>
2022-10-30 17:18:12 +0100

    nc

--- blog/tests.py
+++ blog/tests.py
@@ -1,10 +1,11 @@
 from django.test import TestCase
-from . models import Post 
+from . models import Post
 
 
 class ModelTesting (TestCase):
     def setUp(self):
-        self.blog = Post.objects.create(title='django', author='django', slug='django')   
+        self.blog = Post.objects.create(title='django', author='django', slug='django')
+
     def test_post_model(self):
         d = self.blog
         self.assertTrue(isinstance(d, Post))
